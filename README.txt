b1f212bdd4fe66dc8f7d47245fc027d381bb853b
pvmontenegro1 <pvmontenegro1@espe.edu.ec>
2024-08-17 23:24:18 -0500

    Subiendo proyecto con múltiples carpetas

---
+++ README.txt
@@ -0,0 +1,10 @@
+Primero debemos saber que este proyecto esta elaborado en Spring Boot
+Lo que se desea realizar es una aplicacion de videos streaming
+los microservicios utilizados son de catalogo de la pelicula y el streaming de la pelicula que se desarrolla de forma local
+Se necesita tener instalado un IDE como Itellij IDEA.
+Posteriormente tenemos correr cada uno de los micro servicios desde la parte inferior del IDE donde observamos SERVICIOS
+Luego procedemos a probar los microservicios es importante tener una carpeta llamada Stream preferentemente en el disco C
+en donde 
+deberemos tener videos descargados en formato mp4 y con esto lograremos que la aplicacipon al momento de correrla 
+se muestren los videos descargados con los nombres que tengamos en la computadora y luego en el IDE en la clase de Catalog, 
+en la aplicacion no se usa Docker se usa un API GETWAY mediante en el cual se ejecutarán desde el pruerto 8081
---
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/genuinecoder/movie_catalog_service/controller/MovieInfoController.java
@@ -0,0 +1,29 @@
+package com.genuinecoder.movie_catalog_service.controller;
+
+import com.genuinecoder.movie_catalog_service.model.MovieInfo;
+import com.genuinecoder.movie_catalog_service.model.MovieInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MovieInfoController {
+    @Autowired
+    private MovieInfoRepository repository;
+    @PostMapping("/movie-info/save")
+    public List<MovieInfo> saveAll(@RequestBody List<MovieInfo> movieInfoList) {
+        return repository.saveAll(movieInfoList);
+
+    }
+    @GetMapping("/movie-info/list")
+    public List<MovieInfo> getAll() {
+        return repository.findAll();
+    }
+    @GetMapping("/movie-info/find-path-by-id/{movieInfoId}")
+    public String findPathById(@PathVariable Long movieInfoId) {
+        var videoInfoOptional = repository.findById(movieInfoId);
+        return videoInfoOptional.map(MovieInfo::getPath).orElse(null);
+    }
+
+}
